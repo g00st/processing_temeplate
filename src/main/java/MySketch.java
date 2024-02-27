d9c3b1b64120894b0c3ad2a462e05989cb8fda32
geist <julian.geist.2000@gmail.com>
2024-02-27 16:24:42 +0100

    fug

--- src/main/java/MySketch.java
+++ src/main/java/MySketch.java
@@ -1,8 +1,23 @@
 import processing.core.PApplet;
+import processing.opengl.PShader;
+import controlP5.*;
+
 
 public class MySketch extends PApplet {
+	ControlP5 cp5;
 	public void setup() {
 		this.getSurface().setResizable(true);
+		cp5 = new ControlP5(this);
+		cp5.addSlider("maxframe")
+				.setPosition(10, 10)
+				.setRange(1, 10)
+				.setValue(5)
+				.setSize(100, 20);
+		cp5.addSlider("decay")
+				.setPosition(10, 40)
+				.setRange(0.001f, 0.005f)
+				.setValue(5)
+				.setSize(100, 20);
 	}
 
 	public void settings() {
